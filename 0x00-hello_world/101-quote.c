62bbbc54d4534c9207d35207023ded2b9b246b3c
EZEIYI CLEMENT <zealotclems@gmail.com>
2022-09-08 19:17:20 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>                                                            
-#include <unistd.h>                                                           
-/**                                                                           
- * main - entry point
- * Description: prints and that piece..                                       
- * Return: 1 if sucessful                                                      
- */                                                                           
-int main(void)                                                                
-{                                                               
-	        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);                                                                                                                              return (1);                                                           
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 1
+ */
+int main(void)
+{
+		int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+			write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",s);
+			return (1);
 }
