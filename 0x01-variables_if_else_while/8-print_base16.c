391c371cb4aaa9a6ff3f3f52c381bb0b61acae85
EZEIYI CLEMENT <zealotclems@gmail.com>
2022-09-09 20:26:51 -0700

    Task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,13 @@ int main(void)
 {
 	int n = '0';
 	int a_to_f = 'a';
+
 	while (n <= '9') /*print 0-9*/
 	{
 		putchar(n);
 		n++;
 	}
+
 	while (a_to_f <= 'f') /*print a-f to finish hexbase*/
 	{
 		putchar(a_to_f);
