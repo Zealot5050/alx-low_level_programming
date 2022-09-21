d147be00c19cf8e55dd40b875e331c1d7235d2b9
EZEIYI CLEMENT <zealotclems@gmail.com>
2022-09-21 02:11:38 -0700

    Main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
-#ifndef HOLBERTON_H
+#ifndef MAIN_H
 
-#define HOLBERTON_H
+#define MAIN_H
 
 
+
+int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
@@ -14,17 +16,19 @@ int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
 
-char *string_toupper(char *s);
+char *string_toupper(char *a);
 
-char *cap_string(char *s);
+char *cap_string(char *a);
 
-char *leet(char *str);
+char *leet(char *a);
 
-char *rot13(char *str);
+char *rot13(char *a);
 
 void print_number(int n);
 
-int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
 
 
 
