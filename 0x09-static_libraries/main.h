67b5ad70359448ac687fdb892e2122c838ea3ffa
EZEIYI CLEMENT <zealotclems@gmail.com>
2022-10-01 03:47:20 -0700

    main

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,8 +2,6 @@
 
 #define FILE_MAIN
 
-
-
 int _putchar(char c);
 
 int _islower(int c);
@@ -43,7 +41,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
-
-
 
 #endif
