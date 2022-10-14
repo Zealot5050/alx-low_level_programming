7ad048b7b9b9e463252652b17557bc342c7966b2
EZEIYI CLEMENT <zealotclems@gmail.com>
2022-10-14 06:27:11 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -12,6 +12,7 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	va_start(list, format);
+
 	if (format)
 	{
 		while (format[i])
@@ -41,6 +42,7 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
+
 	printf("\n");
 	va_end(list);
 }
