a975e52b14b94f7f7cac13b85e397b179d9f5d3d
EZEIYI CLEMENT <zealotclems@gmail.com>
2022-10-14 06:50:42 -0700

    header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,18 +1,13 @@
-#ifndef VAR_FUNC_H
-#define VAR_FUNC_H
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
 
 
-#include <stdio.h>
-#include <stdarg.h>
+void print_name(char *name, void (*f)(char *));
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 
-int sum_them_all(const unsigned int n, ...);
-
-void print_numbers(const char *separator, const unsigned int n, ...);
-
-void print_strings(const char *separator, const unsigned int n, ...);
-
-void print_all(const char * const format, ...);
+int int_index(int *array, int size, int (*cmp)(int));
 
 
 #endif
